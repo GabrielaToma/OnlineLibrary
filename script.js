efdbbaa0670508993d368df5ba2158fccaa379a5
GabrielaToma <toma.gabriela00@yahoo.com>
2024-10-24 23:53:44 +0300

    v1

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+
+//enabling tooltips with bootstrap
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+console.log("hello world2");
